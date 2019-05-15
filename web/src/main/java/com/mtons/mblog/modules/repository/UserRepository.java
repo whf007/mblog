daea320052ea769702766ee372201f05e70387ed
wanghefei <274418259@qq.com>
2019-05-15 13:35:51 +0800

    dao层添加

---
+++ web/src/main/java/com/mtons/mblog/modules/repository/UserRepository.java
@@ -0,0 +1,37 @@
+/*
++--------------------------------------------------------------------------
+|   Mblog [#RELEASE_VERSION#]
+|   ========================================
+|   Copyright (c) 2014, 2015 mtons. All Rights Reserved
+|   http://www.mtons.com
+|
++---------------------------------------------------------------------------
+*/
+package com.mtons.mblog.modules.repository;
+
+import com.mtons.mblog.modules.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+
+/**
+ * @author langhsu
+ */
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+
+    @Modifying
+    @Query("update User set posts = posts + :increment where id = :id")
+    int updatePosts(@Param("id") long id, @Param("increment") int increment);
+
+    @Modifying
+    @Query("update User set comments = comments + :increment where id in (:ids)")
+    int updateComments(@Param("ids") Collection<Long> ids, @Param("increment") int increment);
+
+}
