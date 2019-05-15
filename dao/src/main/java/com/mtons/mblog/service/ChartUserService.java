d3980612165071156115dc40d540ca165fa948cf
whf007 <274418259@qq.com>
2019-05-15 22:50:00 +0800

    整合聊天websocket

--- dao/src/main/java/com/mtons/mblog/service/ChartUserService.java
+++ dao/src/main/java/com/mtons/mblog/service/ChartUserService.java
@@ -1,12 +1,12 @@
-package com.mtons.mblog.dao;/**
+package com.mtons.mblog.service;/**
  * Created by Administrator on 2019/5/15.
  */
 
 import com.mtons.mblog.config.DatabaseContextHolder;
 import com.mtons.mblog.enums.DatabaseType;
-import com.mtons.mblog.mapper.UserMapper;
-import com.mtons.mblog.pojo.User;
-import org.mybatis.spring.support.SqlSessionDaoSupport;
+import com.mtons.mblog.mapper.ChatUserRecordMapper;
+import com.mtons.mblog.pojo.ChatUserRecord;
+import com.mtons.mblog.pojo.ChatUserRecordExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,11 +19,12 @@ import java.util.List;
  * @create: on 2019/5/15.
  **/
 @Component
-public class UserDao   {
+public class ChartUserService {
     @Autowired
-    UserMapper userMapper;
-    public List<User> selectAll(){
+    ChatUserRecordMapper chatUserRecordMapper;
+    public List<ChatUserRecord> selectAll(){
         DatabaseContextHolder.setDatabaseType(DatabaseType.mytestdb2);
-        return userMapper.selectAll();
+        ChatUserRecordExample example = new ChatUserRecordExample();
+        return chatUserRecordMapper.selectByExample(example);
     }
 }
