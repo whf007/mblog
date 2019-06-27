ac2e78a266da10830e36eaa0f005724d59aa5538
whf007 <274418259@qq.com>
2019-06-27 19:03:59 +0800

    upload

--- web/src/main/java/com/mtons/mblog/web/controller/site/record/RadioController.java
+++ web/src/main/java/com/mtons/mblog/web/controller/site/record/RadioController.java
@@ -30,8 +30,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.Date;
 import java.util.HashMap;
 
@@ -91,14 +91,71 @@ public class RadioController extends BaseController {
         }
         model.put("owner", owner);
     }
+    @GetMapping("download")
+    public String download(HttpServletRequest request,HttpServletResponse response) throws Exception {
+        String code = request.getParameter("code");
+        String message = request.getParameter("message");
+        String requestId = request.getParameter("requestId");
+        String audioTime = request.getParameter("audioTime");
+        log.info(message + "----返回信息");
+        //获得请求文件名
+        String filename = request.getParameter("filename");
+        System.out.println(filename);
+        //设置文件MIME类型
+        response.setContentType(request.getServletContext().getMimeType("1.txt"));
+        //设置Content-Disposition
+        response.setHeader("Content-Disposition", "attachment;filename=" + "1.txt");
+        //读取目标文件，通过response将目标文件写到客户端
+        //获取目标文件的绝对路径
+//        String fullFileName = request.getServletContext().getRealPath("/download/" + filename);
+        //System.out.println(fullFileName);
+        //读取文件
+//        InputStream in = new FileInputStream(fullFileName);
+        OutputStream out = response.getOutputStream();
+
+        //写文件
+//        int b;
+//        while ((b = in.read()) != -1) {
+            out.write("123123123".getBytes());
+//        }
+
+//        in.close();
+        out.close();
+        return "success";
+    }
     @PostMapping("result")
     @ResponseBody
-    public String result(HttpServletRequest request) {
+    public String result(HttpServletRequest request) throws Exception {
+        FileOutputStream outSTr = null;
+        BufferedOutputStream Buff = null;
+        FileWriter fw = null;
+        try {
         String code = request.getParameter("code");
         String message = request.getParameter("message");
         String requestId = request.getParameter("requestId");
         String audioTime = request.getParameter("audioTime");
         log.info(message +"----返回信息");
+        //经过测试：ufferedOutputStream执行耗时:1,1，1 毫秒
+        outSTr = new FileOutputStream(new File("/download/" + requestId + ".txt"));
+        Buff = new BufferedOutputStream(outSTr);
+        long begin0 = System.currentTimeMillis();
+        Buff.write(message.getBytes());
+        Buff.flush();
+        Buff.close();
+        long end0 = System.currentTimeMillis();
+        System.out.println("BufferedOutputStream执行耗时:" + (end0 - begin0) + " 毫秒");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fw.close();
+                Buff.close();
+                outSTr.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
         // 根据回调结果中的任务号找到订单
 //        Order order = orderService.get(requestId);
 //        if (order.getResult() == null && code.equals("0")) {
